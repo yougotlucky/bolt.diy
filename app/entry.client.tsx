900c2305d92fb8f4c26f5113e52758c053ec8116
yougotlucky <aleemulla85@gmail.com>
2024-12-29 16:29:14 +0530

    Update entry.client.tsx

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,7 +1,7 @@
-import { RemixBrowser } from '@remix-run/react';
-import { startTransition } from 'react';
+@@ -3,5 +3,5 @@ import { startTransition } from 'react';
 import { hydrateRoot } from 'react-dom/client';
 
 startTransition(() => {
+  hydrateRoot(document, <RemixBrowser />);
   hydrateRoot(document.getElementById('root')!, <RemixBrowser />);
 });
